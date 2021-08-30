f677f3f9085a231228d388fa118ccef246200b77
Daniel Dozie <31423288+badt0men@users.noreply.github.com>
2021-08-30 04:34:04 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    FAUNA_ADMIN_KEY : "fnAERvkMo2ACScEIudLcSUWc56kaD8H8DWuvk3QJ",
-    FAUNA_SERVER_KEY: 'fnAERv0RPfACSxdxiw8FfDMeHIgJaom_X2mHPZAP',
-    GRAPHQL_ENDPOINT: "https://graphql.fauna.com/graphql",
-  },
-  basePath : '',
-}
