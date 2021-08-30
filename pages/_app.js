4e80d966582f8667261c5814fcab1f9303b4ac86
niel63 <placmedia@gmail.com>
2021-08-30 04:15:31 +0100

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,15 @@ import client from '../lib/apolloClient'
 import { ThemeProvider } from 'tailwind-darkmode-toggle';
 import ReactAudioPlayer from 'react-audio-player'
 
+
 function MyApp({ Component, pageProps }) {
   return (<>
     <ThemeProvider>
       <ApolloProvider client={client}>
         <Component {...pageProps} />
         <ReactAudioPlayer
-          src="/assets/audio/Snowchild.mp3"
-          autoPlay='true'
+          src="/assets/audio/Snowball.mp3"
+          autoPlay={true}
         />
       </ApolloProvider>
     </ThemeProvider>
