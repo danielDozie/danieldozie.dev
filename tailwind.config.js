24e9e24374219d95a79bbbc93595e5a21d719a28
Daniel Dozie <31423288+badt0men@users.noreply.github.com>
2021-09-06 06:21:41 +0100

    .......

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
