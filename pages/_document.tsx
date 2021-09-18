b8a9bf4fc8cca15019ad0da5f92f0767dccea8ac
niel63 <placmedia@gmail.com>
2021-09-18 14:58:19 +0100

    ...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ThemeScriptTag } from 'tailwind-darkmode-toggle';
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -12,7 +11,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <ThemeScriptTag />
         <Head>
-          
+          <link rel="shortcut icon" href="" />
         </Head>
         <body>
           <Main />
