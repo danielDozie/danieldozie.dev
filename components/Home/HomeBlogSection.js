bd501a50f9d724b8fbb42302279601e68b48a95a
niel63 <placmedia@gmail.com>
2021-09-18 05:00:29 +0100

    .....

--- components/Home/HomeBlogSection.js
+++ components/Home/HomeBlogSection.js
@@ -1,67 +1,100 @@
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const HomeBlogSection = () => {
-    const Icons = [
-        {
-            name: 'react',
-            color: 'bg-purple-500',
-            title: 'Another one',
-            description: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
-        },
-        {
-            name: 'react',
-            color: 'bg-purple-500',
-            title: 'Another one',
-            description: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
-        },
-        {
-            name: 'react',
-            color: 'bg-purple-500',
-            title: 'Another one',
-            description: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
-        },
-        {
-            name: 'react',
-            color: 'bg-purple-500',
-            title: 'Another one',
-            description: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
-        }]
-    return (
-        <>
-            <div className="max-w-7xl mx-auto md:py-8 sm:px-6 lg:py-16 h-full">
-                <div className="flex flex-col justify-start mb-4 md:mb-8 mx-8 md:mx-0">
-                    <h1 className="font-archivo text-4xl  font-semibold text-left bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-yellow-400 to-yellow-800 mt-20 md:mt-0">Yeah! I&apos;ve got a blog too</h1>
-                    <p className="text-pink-500 text-sm pt-2 dark:text-gray-300">Catch up with some really cool posts on the latest tech trends</p>
+  const Icons = [
+    {
+      id: 1,
+      name: "react",
+      color: "bg-purple-500",
+      title: "Another one",
+      description:
+        "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+    },
+    {
+      id: 2,
+      name: "react",
+      color: "bg-purple-500",
+      title: "Another one",
+      description:
+        "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+    },
+    {
+      id: 3,
+      name: "react",
+      color: "bg-purple-500",
+      title: "Another one",
+      description:
+        "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+    },
+    {
+      id:4,
+      name: "react",
+      color: "bg-purple-500",
+      title: "Another one",
+      description:
+        "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+    },
+  ];
+  return (
+    <>
+      <div className="max-w-7xl mx-auto md:py-8 sm:px-6 lg:py-16 h-full">
+        <div className="flex flex-col justify-start mb-4 md:mb-8 mx-8 md:mx-0">
+          <h1 className="font-archivo text-4xl  font-semibold text-left bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-yellow-400 to-yellow-800 mt-20 md:mt-0">
+            Yeah! I&apos;ve got a blog too
+          </h1>
+          <p className="text-pink-500 text-sm pt-2 dark:text-gray-300">
+            Catch up with some really cool posts on the latest tech trends
+          </p>
+        </div>
+
+        <div>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:mt-0 lg:grid-cols-2  p-4 md:p-10 ">
+              {Icons.map(({ name, title, description, id }) => (
+                <div
+                  className={`flex flex-col md:flex-row md:justify-between py-4 px-4 rounded-lg shadow-2xl  dark:border-gray-600 bottom-2`}
+                  key={id}
+                >
+                  <div className="flex h-full bg-center w-full mr-8 mb-4 ">
+                    <Image
+                      src={"/assets/images/happy.gif"}
+                      width={500}
+                      height={"300px"}
+                      className="rounded-lg"
+                      alt={name}
+                    />
+                  </div>
+                  <div className="w-full flex flex-col ">
+                    <h1 className="font-mitr font-semibold text-sm md:text-lg bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+                      {title}
+                    </h1>
+                    <p className="font-mitr font-light text-sm mb-5 text-gray-700 dark:text-gray-200">
+                      {" "}
+                      {description?.substr(0, 110)}{" "}
+                      <Link href={"/"}>...Read more</Link>
+                    </p>
+                    <hr />
+                    <div className="flex text-xs font-thin mt-2 justify-between">
+                      <p className="text-gray-700 dark:text-gray-300">
+                        Published:{" "}
+                        <span className="text-pink-500">Sept. 2021</span>
+                      </p>
+                      <p className="text-gray-700 dark:text-gray-300">
+                        Author: <span className="text-pink-500">Badt0men</span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-
-                <div>
-                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:mt-0 lg:grid-cols-2  p-4 md:p-10 ">
-                        {Icons.map(({ name, title, description,i }) => (
-                            <div className={`flex flex-col md:flex-row md:justify-between py-4 px-4 rounded-lg shadow-2xl  dark:border-gray-600 bottom-2`} key={i}>
-                                <div className="flex h-full bg-center w-full mr-8 mb-4 ">
-                                    <Image src={'/assets/images/happy.gif'} width={500} height={'300px'} className="rounded-lg" alt={name} />
-
-                                </div>
-                                <div className="w-full flex flex-col ">
-                                    <h1 className="font-mitr font-semibold text-sm md:text-lg bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">{title}</h1>
-                                    <p className="font-mitr font-light text-sm mb-5 text-gray-700 dark:text-gray-200"> {description?.substr(0, 110)} <Link href={'/'}>...Read more</Link></p>
-                                    <hr />
-                                    <div className="flex text-xs font-thin mt-2 justify-between">
-                                        <p className="text-gray-700 dark:text-gray-300">Published: <span className="text-pink-500">Sept. 2021</span></p>
-                                        <p className="text-gray-700 dark:text-gray-300">Author: <span className="text-pink-500">Badt0men</span></p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    </div>
-                    <div className="justify-center text-center py-8 md:py-0 md:pt-16">
-                        <button className="px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Load More...</button>
-                    </div>
-                </div>
-        </>
-    )
-}
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="justify-center text-center py-8 md:py-0 md:pt-16">
+          <button className="px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+            Load More...
+          </button>
+        </div>
+    </>
+  );
+};
--- components/Nav.js
+++ components/Nav.js
@@ -1,107 +1,144 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { Toggle } from '../components/Toggle'
-import { Dialog } from '@headlessui/react';
-import { Icon } from '@iconify/react';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Toggle } from "../components/Toggle";
+import { Dialog } from "@headlessui/react";
+import { Icon } from "@iconify/react";
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const menuItems = [
-    { name: 'About Me', href: '/about', current: true },
-    { name: 'Resume', href: '/resume', current: false },
-    { name: 'Services', href: '/services', current: false },
-    { name: 'Projects', href: '/projects', current: false },
-    { name: 'Blog', href: '/blog', current: false },
-    { name: 'Contact', href: '/contact', current: false },
-]
+  { name: "About Me", href: "/about", current: true },
+  { name: "Resume", href: "/resume", current: false },
+  { name: "Services", href: "/services", current: false },
+  { name: "Projects", href: "/projects", current: false },
+  { name: "Blog", href: "/blog", current: false },
+  { name: "Contact", href: "/contact", current: false },
+];
 
 export const Nav = () => {
-
-    return (
-        <>
-            <header className="h-24 flex items-center w-full bg-transparent z-40 fixed backdrop-filter backdrop-blur-lg">
-                <div className="container mx-auto px-6 flex items-center justify-between">
-                    <Link href={'/'} passHref>
-                        <div className="uppercase  font-black text-3xl flex items-center font-archivo">
-                            <Image className="border-2" src={'/assets/images/headIcon.svg'} height={40} width={42} alt={'logo'} />
-                            <div className="flex flex-col">
-                                <span className="text-md ml-4 mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
-                                    C.D.E
-                                </span>
-                                <span className="font-mitr capitalize font-light text-xs bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">chidozie Daniel Eluchie</span>
-                            </div>
-                        </div>
-                    </Link>
-                    <div className="flex items-center">
-                        <nav className=" text-gray-800 dark:text-white uppercase text-normal lg:flex items-center hidden">
-                            {menuItems.map((item) => (
-                                <div className="py-2 px-6 flex hover:text-gray-400" key={item.name}>
-                                    <Link href={item.href} className={classNames(item.current ? 'text-gray-400' : '')} aria-current={item.current ? 'page' : undefined}>{item.name}</Link>
-                                </div>
-                            ))}
-                        </nav>
-                        <Toggle />
-                        <MobileIcon />
-                    </div>
+  return (
+    <>
+      <header className="h-24 flex items-center w-full bg-transparent z-40 fixed backdrop-filter backdrop-blur-lg">
+        <div className="container mx-auto px-6 flex items-center justify-between">
+          <Link href={"/"} passHref>
+            <div className="uppercase  font-black text-3xl flex items-center font-archivo">
+              <Image
+                className="border-2"
+                src={"/assets/images/headIcon.svg"}
+                height={40}
+                width={42}
+                alt={"logo"}
+              />
+              <div className="flex flex-col">
+                <span className="text-md ml-4 mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+                  C.D.E
+                </span>
+                <span className="font-mitr capitalize font-light text-xs bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+                  chidozie Daniel Eluchie
+                </span>
+              </div>
+            </div>
+          </Link>
+          <div className="flex items-center">
+            <nav className=" text-gray-800 dark:text-white uppercase text-normal lg:flex items-center hidden">
+              {menuItems.map((item) => (
+                <div
+                  className="py-2 px-6 flex hover:text-gray-400"
+                  key={item.name}
+                >
+                  <Link
+                    href={item.href}
+                    className={classNames(item.current ? "text-gray-400" : "")}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Link>
                 </div>
-            </header>
-        </>
-    )
-}
-
-
+              ))}
+            </nav>
+            <Toggle />
+            <MobileIcon />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export const MobileIcon = () => {
-    const [open, setOpen] = useState(false);
-    const closeMenu = () => {
-        setOpen(false)
-    }
-    const openMenu = () => {
-        setOpen(true);
-    }
+  const [open, setOpen] = useState(false);
+  const closeMenu = () => {
+    setOpen(false);
+  };
+  const openMenu = () => {
+    setOpen(true);
+  };
 
-    return (
-        <>
-            <Icon className="text-4xl text-gray-800 dark:text-gray-400 -mr-2" icon="carbon:overflow-menu-vertical" onClick={openMenu} />
-            <Dialog as="div" open={open} onClose={closeMenu} className=" inset-0 h-screen w-full overflow-y-auto bg-transparent z-40 fixed backdrop-filter backdrop-blur-lg">
-                <Dialog.Overlay />
-                <Dialog.Title>
-                    <div>
-                        <div className="flex flex-row justify-between p-5 mt-2">
-                            <div className="text-2xl font-mitir font-semibold text-gray-800 dark:text-gray-200">
-                                <h1>Menu</h1>
-                            </div>
+  return (
+    <>
+      <Icon
+        className="text-4xl text-gray-800 dark:text-gray-400 -mr-2 md:hidden "
+        icon="carbon:overflow-menu-vertical"
+        onClick={openMenu}
+      />
+      <Dialog
+        as="div"
+        open={open}
+        onClose={closeMenu}
+        className=" inset-0 h-screen w-full overflow-y-auto bg-transparent z-40 fixed backdrop-filter backdrop-blur-lg"
+      >
+        <Dialog.Overlay />
+        <Dialog.Title>
+          <div>
+            <div className="flex flex-row justify-between p-5 mt-2">
+              <div className="text-2xl font-mitir font-semibold text-gray-800 dark:text-gray-200">
+                <h1>Menu</h1>
+              </div>
 
-                            <div>
-                                <Icon className="text-4xl text-gray-800 dark:text-gray-200" icon="eva:close-fill" onClick={closeMenu} />
-                            </div>
-                        </div>
-                            <div className="uppercase font-black text-3xl flex items-center font-archivo justify-center -mt-4">
-                                <Image className="border-2" src={'/assets/images/headIcon.svg'} height={40} width={42} alt={'logo'} />
-                                <div className="flex flex-col">
-                                    <span className="text-md ml-4 mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
-                                        C.D.E
-                                    </span>
-                                    <span className="font-mitr capitalize font-light text-xs bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">chidozie Daniel Eluchie</span>
-                                </div>
-                            </div>
-                    </div>
-                </Dialog.Title>
-                <Dialog.Description>
-                    <nav className="font-mitr text-gray-800 dark:text-gray-200 uppercase font-normal text-xl mt-8">
-                        {menuItems.map(mobileMenu => (
-                            <div onClick={closeMenu} className="py-2 flex hover:text-gray-400 justify-center text-center align-center" key={mobileMenu.name}>
-                                <Link href={mobileMenu.href}>
-                                {mobileMenu.name}
-                                </Link>
-                            </div>
-                        ))}
-                    </nav>
-                </Dialog.Description> 
-            </Dialog>
-        </>)
-}
-
+              <div>
+                <Icon
+                  className="text-4xl text-gray-800 dark:text-gray-200"
+                  icon="eva:close-fill"
+                  onClick={closeMenu}
+                />
+              </div>
+            </div>
+            <div className="uppercase font-black text-3xl flex items-center font-archivo justify-center -mt-4">
+              <Image
+                className="border-2"
+                src={"/assets/images/headIcon.svg"}
+                height={40}
+                width={42}
+                alt={"logo"}
+              />
+              <div className="flex flex-col">
+                <span className="text-md ml-4 mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+                  C.D.E
+                </span>
+                <span className="font-mitr capitalize font-light text-xs bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+                  chidozie Daniel Eluchie
+                </span>
+              </div>
+            </div>
+          </div>
+        </Dialog.Title>
+        <Dialog.Description>
+          <nav className="font-mitr text-gray-800 dark:text-gray-200 uppercase font-normal text-xl mt-8">
+            {menuItems.map((mobileMenu) => (
+              <div
+                onClick={closeMenu}
+                className="py-2 flex hover:text-gray-400 justify-center text-center align-center"
+                key={mobileMenu.name}
+              >
+                <Link href={mobileMenu.href}>{mobileMenu.name}</Link>
+              </div>
+            ))}
+          </nav>
+        </Dialog.Description>
+      </Dialog>
+    </>
+  );
+};
