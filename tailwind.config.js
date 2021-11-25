54b016649c4c93195dba0cb130b74b699d14f6dd
niel63 <placmedia@gmail.com>
2021-11-25 22:56:03 +0100

    revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-],
-darkMode: 'class', 
+  darkMode: "class",
+  purge:{
+    content:["./pages/**/*.tsx", "./components/**/*.tsx"],
+    safelist: [
+      "dark",
+      "bg-purple-500",
+      "bg-purple-300",
+      "bg-pink-500",
+      "bg-pink-300",
+      "bg-green-500",
+      "bg-green-300",
+      "bg-blue-500",
+      "bg-blue-300",
+      "bg-yellow-500",
+      "bg-yellow-300"
+    ]},
   theme: {
     extend: {},
     fontFamily: {
