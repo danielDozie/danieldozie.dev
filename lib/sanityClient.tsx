ee304504ed2917caf995b8958f22ca4265b2af27
badt0men <placmedia@gmail.com>
2022-09-10 02:37:56 +0100

    Updated Sanity Token

--- lib/sanityClient.tsx
+++ lib/sanityClient.tsx
@@ -4,7 +4,7 @@ const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2021-03-25',
-  token: process.env.NEXT_PUBLIC_SANITY_DEV_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   useCdn: false,
 })
 
